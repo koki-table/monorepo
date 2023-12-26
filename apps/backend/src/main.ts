ad914ba635c422a9001a37438eee15a71d65da57
koki-table <koki.s0327@icloud.com>
2023-12-26 23:23:27 +0900

    #3 backend swagger追加

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as fs from 'fs'
+import { dump } from 'js-yaml'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('sample example')
+    .setDescription('The sample API description')
+    .setVersion('1.0')
+    .addTag('sample')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync("./swagger-spec.yaml", dump(document, {}));
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
