2990468f57ae015eefcc7098201d00f480084a53
koki-table <koki.s0327@icloud.com>
2024-01-07 12:47:48 +0900

    config: path調整

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,5 @@
 // tailwind config is required for editor support
-import sharedConfig from "@monorepo/configs/tailwind/tailwind.config";
+import sharedConfig from "@monorepo/configs/tailwind/tailwind.config.js";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "presets"> = {
